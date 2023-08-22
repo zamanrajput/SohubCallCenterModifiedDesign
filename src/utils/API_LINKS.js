dc945f6d1dc8d95355f409083fc93521bcc89279
zaman <zamanbhatti275@gmail.com>
2023-08-22 13:56:45 +0500

    links updated

--- src/utils/API_LINKS.js
+++ src/utils/API_LINKS.js
@@ -2,4 +2,4 @@
 
 export const BASE_URL = "http://202.59.208.112";
 
-export const URL_LOGIN =  "http://202.59.208.112/contact_center/authentication/login_api";
+export const URL_LOGIN =  "https://api-cc.sohub.com.bd/authentication/login_api";
